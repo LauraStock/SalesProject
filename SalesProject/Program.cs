8b582e90cfde2beaff387002d36700dfdfcff737
LauraStock <l.stockcaldwell@gmail.com>
2021-10-29 11:28:33 +0100

    Final Commit

--- SalesProject/Program.cs
+++ SalesProject/Program.cs
@@ -16,6 +16,18 @@ namespace SalesProject
 
             using (connection = MySqlUtils.GetConnection())
             {
+
+                /* RUN THIS CODE TO CHECK THE DB CONNECTION
+                //opening the db connection and creating the sales db if it does not exist.
+                connection.Open();
+                string path = "./" + @"\Data\dbSetup.sql";
+                MySqlUtils.RunSchemaFile(path, connection);
+                connection.Ping();
+                bool connectionOpen = connection.Ping();
+                Console.WriteLine($"Connection status: {connection.State} \nPing successfull: {connectionOpen} \nDB Version: {connection.ServerVersion}");
+                */
+
+                
                 SalesRepository repo = new SalesRepository(connection);
                 SalesService service = new SalesService(repo);
                 SalesController controller = new SalesController(service);
@@ -23,17 +35,9 @@ namespace SalesProject
                 Menu menu = new Menu(controller);
                 Console.WriteLine(menu);
                 menu.MenuLoop();
+                
 
-                //Console.WriteLine(repo.Exists());
-                /*
-                //opening the db connection and creating the sales db if it does not exist.
-                connection.Open();
-                string path = "./" + @"\Data\dbSetup.sql";
-                MySqlUtils.RunSchemaFile(path, connection);
-                */
-                //connection.Ping();
-                //bool connectionOpen = connection.Ping();
-                //Console.WriteLine($"Connection status: {connection.State} \nPing successfull: {connectionOpen} \nDB Version: {connection.ServerVersion}");
+
 
             }
         }
